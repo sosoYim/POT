6f0703ddd05197f9d186e512dd843c1e26156cfe
hustle-dev <dlwoabsdk@gmail.com>
2021-11-11 20:08:20 +0900

    feat: install dotenv library

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const users = require('./model/users');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.send('hi');
+  // res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`)); // port, callback
