c9b3f95f7b83339bc83cdc805cb18c8a23d10506
hustle-dev <dlwoabsdk@gmail.com>
2021-11-12 16:57:19 +0900

    feat: apply join in register and create query folder for query.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use('/user', authRoutes);
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.send('hi');
 });
 
