9975e45ca8e4b18a6b4f16fbb9e80e11ac7f2e49
hustle-dev <dlwoabsdk@gmail.com>
2021-11-12 13:00:39 +0900

    feat: folder directory architect

---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const users = require('../model/users');
+
+router.post('/signin', (req, res) => {
+  const { userid, password } = req.body;
+
+  console.log(req.body);
+
+  // 401 Unauthorized
+  if (!userid || !password)
+    return res.status(401).send({ error: '사용자 아이디 또는 패스워드가 전달되지 않았습니다.' });
+
+  const user = users.findUser(userid, password);
+  console.log('사용자 정보:', user);
+
+  // 401 Unauthorized
+  if (!user) return res.status(401).send({ error: '등록되지 않은 사용자입니다.' });
+
+  // 토큰 생성
+  const accessToken = jwt.sign({ userid }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '1d',
+  });
+
+  // 쿠키에 토큰 설정(http://expressjs.com/ko/api.html#res.cookie)
+  res.cookie('accessToken', accessToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7d
+    httpOnly: true,
+  });
+
+  // 로그인 성공
+  res.send({ userid, encryptedId: user.encryptedId });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-const users = require('./model/users');
+const authRoutes = require('./routes/auth');
 
 require('dotenv').config();
 
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/user', authRoutes);
 
 const auth = (req, res, next) => {
   // 토큰이 리퀘스트의 Authorization 헤더를 통해 전달되면 req.headers.authorization으로 전달받고
@@ -30,36 +31,6 @@ const auth = (req, res, next) => {
     return res.redirect('/signin');
   }
 };
-
-app.post('/signin', (req, res) => {
-  const { userid, password } = req.body;
-
-  console.log(req.body);
-
-  // 401 Unauthorized
-  if (!userid || !password)
-    return res.status(401).send({ error: '사용자 아이디 또는 패스워드가 전달되지 않았습니다.' });
-
-  const user = users.findUser(userid, password);
-  console.log('사용자 정보:', user);
-
-  // 401 Unauthorized
-  if (!user) return res.status(401).send({ error: '등록되지 않은 사용자입니다.' });
-
-  // 토큰 생성
-  const accessToken = jwt.sign({ userid }, process.env.JWT_SECRET_KEY, {
-    expiresIn: '1d',
-  });
-
-  // 쿠키에 토큰 설정(http://expressjs.com/ko/api.html#res.cookie)
-  res.cookie('accessToken', accessToken, {
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7d
-    httpOnly: true,
-  });
-
-  // 로그인 성공
-  res.send({ userid, encryptedId: user.encryptedId });
-});
 
 app.get('/*', (req, res) => {
   res.send('hi');
