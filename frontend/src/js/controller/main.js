7fe3bdf78eb807f0abd8315b244ca599f484b951
jhyj0521 <jhyj3636@gmail.com>
2021-11-15 10:23:49 +0900

    feat: Fetch main page data

--- frontend/src/js/controller/main.js
+++ frontend/src/js/controller/main.js
@@ -1,1 +1,19 @@
-console.log(1);
+import axios from 'axios';
+import { setBoards } from '../store/main';
+
+const fetchBoards = async () => {
+  try {
+    const { data } = await axios.get(`/api/boards/list`, {
+      params: {
+        currentPageNo: 1,
+        recordsPerPage: 8,
+      },
+    });
+
+    setBoards(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { fetchBoards };
