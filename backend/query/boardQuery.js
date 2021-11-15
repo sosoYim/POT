ab45cce7df70c538c6af404d8940386ae49ee27c
sosoYim <sohyung.yim@gmail.com>
2021-11-15 13:36:43 +0900

    style: Comment useless files

--- backend/query/boardQuery.js
+++ backend/query/boardQuery.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const boards = JSON.parse(fs.readFileSync('./backend/db/boards.json'));
+// const boards = JSON.parse(fs.readFileSync('./backend/db/boards.json'));
 
-const getBoardList = (currentPageNo, recordsPerPage) => {
-  const boards = JSON.parse(fs.readFileSync('./backend/db/boards.json'));
-  const startIndex = (currentPageNo - 1) * recordsPerPage;
+// const getBoardList = (currentPageNo, recordsPerPage) => {
+//   const boards = JSON.parse(fs.readFileSync('./backend/db/boards.json'));
+//   const startIndex = (currentPageNo - 1) * recordsPerPage;
 
-  return boards.filter((board, index) => index >= startIndex && index < startIndex + recordsPerPage);
-};
+//   return boards.filter((board, index) => index >= startIndex && index < startIndex + recordsPerPage);
+// };
 
-/**
- *
- * @param {string} boardId
- * @returns
- */
-const getBoardById = boardId => boards.find({ boardId });
+// /**
+//  *
+//  * @param {string} boardId
+//  * @returns
+//  */
+// const getBoardById = boardId => boards.find({ boardId });
 
-/**
- * Add board in BOARD dataset
- * @param {object} newBoard
- */
-const setBoard = newBoard => {
-  const newBoardId = boards.length === 0 ? 1 : boards[0].boardId + 1;
-  const regDate = new Date(+new Date() + 3240 * 10000).toISOString().replace('T', ' ').replace(/\..*/, '');
+// /**
+//  * Add board in BOARD dataset
+//  * @param {object} newBoard
+//  */
+// const setBoard = newBoard => {
+//   const newBoardId = boards.length === 0 ? 1 : boards[0].boardId + 1;
+//   const regDate = new Date(+new Date() + 3240 * 10000).toISOString().replace('T', ' ').replace(/\..*/, '');
 
-  const newBoards = [{ boardId: newBoardId, ...newBoard, regDate }, ...boards];
-  fs.writeFileSync('./backend/db/boards.json', JSON.stringify(newBoards));
+//   const newBoards = [{ boardId: newBoardId, ...newBoard, regDate }, ...boards];
+//   fs.writeFileSync('./backend/db/boards.json', JSON.stringify(newBoards));
 
-  return newBoardId;
-};
+//   return newBoardId;
+// };
 
-module.exports = { getBoardList, getBoardById, setBoard };
+// module.exports = { getBoardList, getBoardById, setBoard };
