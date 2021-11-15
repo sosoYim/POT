ccd78ed5d2b76a032944e7651b5bed780af83bb7
sosoYim <sohyung.yim@gmail.com>
2021-11-15 13:32:44 +0900

    fix: Merged with manage
    - need to delete boards.js -> boardsRoutes.js
    - need to delete boardQuery.js -> boardsQuery.js

--- backend/query/boardsQuery.js
+++ backend/query/boardsQuery.js
@@ -5,4 +5,33 @@ const getBoardData = boardId => {
   return boards.find(board => board.boardId === +boardId);
 };
 
-module.exports = { getBoardData };
+const getBoardList = (currentPageNo, recordsPerPage) => {
+  const boards = JSON.parse(fs.readFileSync('./backend/db/boards.json'));
+  const startIndex = (currentPageNo - 1) * recordsPerPage;
+
+  return boards.filter((board, index) => index >= startIndex && index < startIndex + recordsPerPage);
+};
+
+/**
+ *
+ * @param {string} boardId
+ * @returns
+ */
+const getBoardById = boardId => JSON.parse(fs.readFileSync('./backend/db/boards.json')).find({ boardId });
+
+/**
+ * Add board in BOARD dataset
+ * @param {object} newBoard
+ */
+const setBoard = newBoard => {
+  const boards = JSON.parse(fs.readFileSync('./backend/db/boards.json'));
+  const newBoardId = boards.length === 0 ? 1 : boards[0].boardId + 1;
+  const regDate = new Date(+new Date() + 3240 * 10000).toISOString().replace('T', ' ').replace(/\..*/, '');
+
+  const newBoards = [{ boardId: newBoardId, ...newBoard, regDate }, ...boards];
+  fs.writeFileSync('./backend/db/boards.json', JSON.stringify(newBoards));
+
+  return newBoardId;
+};
+
+module.exports = { getBoardData, getBoardList, getBoardById, setBoard };
