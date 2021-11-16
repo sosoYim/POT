956c977071aa830904d6bffcea1db24354888b25
hustle-dev <dlwoabsdk@gmail.com>
2021-11-16 11:06:44 +0900

    feat:edit eye icon position

--- frontend/src/js/event/login.js
+++ frontend/src/js/event/login.js
@@ -1,7 +1,47 @@
 import axios from '../utils/axiosConfig';
 
-const $registerLink = document.querySelector('.login-form__goto-signup');
+const [$emailInput, $passwordInput] = document.querySelectorAll('.login-form__input');
 
-$registerLink.onclick = () => {
-  window.location = 'http://localhost:3000/register';
+const formData = {
+  email: {
+    value: '',
+    validate: false,
+  },
+  password: {
+    value: '',
+    validate: false,
+  },
+};
+
+const setEmailFormData = input => {
+  formData.email.value = input.value;
+  formData.email.validate = true;
+};
+
+const setPasswordFormData = input => {
+  formData.password.value = input.value;
+  formData.password.validate = true;
+};
+
+const initializeValue = input => {
+  formData[input.name].value = '';
+  formData[input.name].validate = false;
+};
+
+$emailInput.onkeyup = ({ target }) => {
+  setEmailFormData(target);
+
+  if (target.value.trim() === '') {
+    initializeValue(target);
+  }
+};
+
+$passwordInput.onkeyup = ({ target }) => {
+  setPasswordFormData(target);
+
+  target.nextElementSibling.nextElementSibling.classList.remove('hidden');
+
+  if (target.value.trim() === '') {
+    initializeValue(target);
+  }
 };
